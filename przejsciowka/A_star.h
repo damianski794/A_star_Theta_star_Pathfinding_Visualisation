98213bfd015a30af8d1b4cb1b07a12576e7c9755
Damian Wysokinski <damianwysokinski@o2.pl>
2019-07-13 17:32:37 +0200

    program do 10 sec mielenia sie wyklada

--- przejsciowka/A_star.h
+++ przejsciowka/A_star.h
@@ -1,23 +1,56 @@
 #pragma once
 
-#include "Node.h"
 #include "window_dimensions.h"
+#include "Node.h"
+
 #include <vector>
 #include <array>
 #include <stack>
+
 #include <iostream>
 
-using namespace std;
+//using namespace std;
+static std::vector<Node> makePath(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)> map, Node dest) {
+	try {
+		std::cout << "Found a path" << std::endl;
+		int x = dest.x;
+		int y = dest.y;
+		std::stack<Node> path;
+		std::vector<Node> usablePath;
 
-static vector<Node> aStar(Node player, Node dest) {
-	vector<Node> empty;
+		while (!(map[x][y].parentX == x && map[x][y].parentY == y)
+			&& map[x][y].x != -1 && map[x][y].y != -1)
+		{
+			path.push(map[x][y]);
+			int tempX = map[x][y].parentX;
+			int tempY = map[x][y].parentY;
+			x = tempX;
+			y = tempY;
+
+		}
+		path.push(map[x][y]);
+
+		while (!path.empty()) {
+			Node top = path.top();
+			path.pop();
+			usablePath.emplace_back(top);
+		}
+		return usablePath;
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+}
+
+static std::vector<Node> aStar(Node player, Node dest) {
+	std::vector<Node> empty;
 	if (isValid(dest.x, dest.y) == false) {
-		cout << "Destination is an obstacle" << endl;
+		std::cout << "Destination is an obstacle" << std::endl;
 		return empty;
 		//Destination is invalid
 	}
 	if (isDestination(player.x, player.y, dest)) {
-		cout << "You are the destination" << endl;
+		std::cout << "You are the destination" << std::endl;
 		return empty;
 		//You clicked on yourself
 	}
@@ -25,7 +58,7 @@ static vector<Node> aStar(Node player, Node dest) {
 
 	//Initialize whole map
 	//Node allMap[50][25];
-	array<array < Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)> allMap;
+	std::array<std::array < Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)> allMap;
 	for (int x = 0; x < (X_MAX / X_STEP); x++) {
 		for (int y = 0; y < (Y_MAX / Y_STEP); y++) {
 			allMap[x][y].fCost = FLT_MAX;
@@ -49,7 +82,7 @@ static vector<Node> aStar(Node player, Node dest) {
 	allMap[x][y].parentX = x;
 	allMap[x][y].parentY = y;
 
-	vector<Node> openList;
+	std::vector<Node> openList;
 	openList.emplace_back(allMap[x][y]);
 	bool destinationFound = false;
 
@@ -62,8 +95,8 @@ static vector<Node> aStar(Node player, Node dest) {
 			//it with a vector, but for now it's still an option, although
 			//not as good as a set performance wise.
 			float temp = FLT_MAX;
-			vector<Node>::iterator itNode;
-			for (vector<Node>::iterator it = openList.begin();
+			std::vector<Node>::iterator itNode;
+			for (std::vector<Node>::iterator it = openList.begin();
 				it != openList.end(); it = next(it)) {
 				Node n = *it;
 				if (n.fCost < temp) {
@@ -115,64 +148,39 @@ static vector<Node> aStar(Node player, Node dest) {
 		}
 	}
 	if (destinationFound == false) {
-		cout << "Destination not found" << endl;
+		std::cout << "Destination not found" << std::endl;
 		return empty;
 	}
 }
 
-static vector<Node> makePath(array<array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)> map, Node dest) {
-	try {
-		cout << "Found a path" << endl;
-		int x = dest.x;
-		int y = dest.y;
-		stack<Node> path;
-		vector<Node> usablePath;
 
-		while (!(map[x][y].parentX == x && map[x][y].parentY == y)
-			&& map[x][y].x != -1 && map[x][y].y != -1)
-		{
-			path.push(map[x][y]);
-			int tempX = map[x][y].parentX;
-			int tempY = map[x][y].parentY;
-			x = tempX;
-			y = tempY;
+//zmienilem std::vector<Node> na void
+static void my_A_Star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)>& mapka, Node& current_Node, Node& destination_Node) {
 
-		}
-		path.push(map[x][y]);
-
-		while (!path.empty()) {
-			Node top = path.top();
-			path.pop();
-			usablePath.emplace_back(top);
-		}
-		return usablePath;
-	}
-	catch (const exception& e) {
-		cout << e.what() << endl;
-	}
-}
-
-static vector<Node> my_A_Star(array<array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)>& mapka, Node& current_Node, Node& destination_Node) {
-
-	vector<Node> empty;
+	//std::cout << "test" << std::endl;
+	std::vector<Node> empty;// return empty;
 	if (my_isValid(destination_Node) == false) {
-		cout << "destination is an obstacle, returning empty path" << endl;
-		return empty;
+		std::cout << "destination is an obstacle, returning empty path" << std::endl;
+		//return empty; zmienilem
+
 	}
 	if (my_isDestination(current_Node, destination_Node)) {
-		cout << "your at the destination, returning empty path" << endl;
-		return empty;
+		std::cout << "your at the destination, returning empty path" << std::endl;
+		//return empty; //zmienilem
 	}
+	
 
 	bool closedList[(X_MAX / X_STEP)][(Y_MAX / Y_STEP)];
-
+	//std::cout << "coping all" << std::endl;
+	//std::array<std::array < Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)> allMap = mapka; //zmienic
+	//std::cout << "all copied" << std::endl;
 	for (int i = 0; i < X_MAX / X_STEP; i++) {
 		for (int j = 0; j < Y_MAX / Y_STEP; j++) {
 			mapka[i][j].fCost = FLT_MAX;
 			mapka[i][j].gCost = FLT_MAX;
 			mapka[i][j].hCost = FLT_MAX;
 			mapka[i][j].parentX = -1;
-			mapka[i][j].parentY = -1;
+			mapka[i][j].parentY = -1; 
 
 			closedList[i][j] = false;
 		}
@@ -187,18 +195,22 @@ static vector<Node> my_A_Star(array<array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_ST
 	mapka[i][j].parentX = i;
 	mapka[i][j].parentY = j;
 
-	vector<Node> openList;
+	std::vector<Node> openList;
+	std::cout << "rozmiar openList przed emplace = " <<openList.size()<< std::endl;
 	openList.emplace_back(mapka[i][j]);
-
+	std::cout << "rozmiar openList po emplace = " << openList.size() << std::endl;
 	bool destination_found = false;
 
 	while (!openList.empty() && openList.size() < (X_MAX / X_STEP)*(Y_MAX / Y_STEP)) {
+		std::cout << "wewnatrz glownej petli !openList.empty() && openList.size()" << std::endl;
 		Node node;
 		do {
 			float temp = FLT_MAX;
-			vector<Node>::iterator itNode;
-			for (vector<Node>::iterator it = openList.begin();
-				it != openList.end(); next(it)) {
+			std::cout << "za temp = flt_max" << std::endl;
+			std::vector<Node>::iterator itNode;
+			for (std::vector<Node>::iterator it = openList.begin();
+				it != openList.end(); ++it) { //zamiast next(it) biore ++it
+				std::cout << "wewnatrz iteratora" << std::endl;
 				Node n = *it;
 				if (n.fCost < temp) {
 					temp = n.fCost;
@@ -207,23 +219,25 @@ static vector<Node> my_A_Star(array<array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_ST
 			}
 			node = *itNode;
 			openList.erase(itNode);
+			std::cout << "wewnatrz do while(my_isValid(node) == false) " << std::endl;
 		} while (my_isValid(node) == false);
 
 		i = node.x;
 		j = node.y;
 		closedList[i][j] = true;
-
+		std::cout << "przed sasiednimi nodami" << std::endl;
 		for (int newX = -1; newX <= 1; newX++) {
 			for (int newY = -1; newY <= 1; newY++) {
 				double gNew, hNew, fNew;
+				std::cout << "sprawdzam sasiadow"<< std::endl;
 				if (my_isValid_xy(i + newX, j + newY, mapka)) {
 					if (my_isDestination(mapka[i + newX][j + newY], destination_Node)) {
 						mapka[i + newX][j + newY].parentX = i;
 						mapka[i + newX][j + newY].parentY = j;
 						destination_found = true;
-						cout << "znaleziono droge!!!" << endl << endl;
+						std::cout << "znaleziono droge!!!" << std::endl << std::endl;
 
-						return makePath(mapka, destination_Node);
+						//return makePath(mapka, destination_Node); zmienilem
 					}
 
 					else if (closedList[i + newX][j + newY] == false) {
@@ -237,6 +251,7 @@ static vector<Node> my_A_Star(array<array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_ST
 							mapka[i + newX][j + newY].hCost = hNew;
 							mapka[i + newX][j + newY].parentX = i;
 							mapka[i + newX][j + newY].parentY = j;
+							openList.emplace_back(mapka[i + newX][j + newY]);
 						}
 					}
 				}
@@ -244,7 +259,16 @@ static vector<Node> my_A_Star(array<array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_ST
 		}
 	}
 	if (destination_found == false) {
-		cout << "destination not found :/" << endl;
-		return empty;
+		std::cout << "destination not found :/" << std::endl;
+		//return empty; zmienilem
 	}
+}
+
+void testowane(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)>& mapka, Node& current_Node, Node& destination_Node) {
+	std::cout <<" mapka[5][5].isObstacle: "<< mapka[5][5].isObstacle << std::endl;
+	std::cout << "current_Node.x: " << current_Node.x << std::endl;
+	std::cout << "current_Node.y: " << current_Node.y << std::endl;
+	std::cout << "destination_Node.x: " << destination_Node.x << std::endl;
+	std::cout << "destination_Node.y: " << destination_Node.y << std::endl;
+	std::cout << std::endl;
 }
--- przejsciowka/przejsciowka.cpp
+++ przejsciowka/przejsciowka.cpp
@@ -8,13 +8,14 @@
 #include <array>
 #include "Square.h"
 #include <vector>
+#include "A_star.h"
 
-using namespace std;
+//using namespace std;
 
 const int columns = Y_MAX / Y_STEP; // ew. na odwrot powinny byc kolumny i wiersze
 const int rows = X_MAX / X_STEP;
 
-void draw_map(array<array<Node, columns>, rows> &mapka,sf::RenderWindow& window){
+void draw_map(std::array<std::array<Node, columns>, rows> &mapka,sf::RenderWindow& window){
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			if (mapka[i][j].isObstacle){
@@ -30,9 +31,9 @@ void draw_map(array<array<Node, columns>, rows> &mapka,sf::RenderWindow& window)
 	}
 }
 
-void draw_path(vector<Node>& path,sf::RenderWindow& window) { //UWAGA na REFERENCJE
+void draw_path(std::vector<Node>& path,sf::RenderWindow& window) { //UWAGA na REFERENCJE
 	if (path.size() == 0) {
-		cout << "sciezka ma dlugosc 0 - brak sciezki" << endl;
+		std::cout << "sciezka ma dlugosc 0 - brak sciezki" << std::endl;
 		return;
 	}
 
@@ -45,7 +46,7 @@ void draw_path(vector<Node>& path,sf::RenderWindow& window) { //UWAGA na REFEREN
 		lines[1].color = sf::Color::Red;
 
 		window.draw(lines);
-		cout << "rysuje linie z " << t->x << "," << t->y << "  do " << t->parentX << "," << t->parentY << endl;
+		//std::cout << "rysuje linie z " << t->x << "," << t->y << "  do " << t->parentX << "," << t->parentY << std::endl;
 	}
 }
 //
@@ -54,17 +55,21 @@ void draw_path(vector<Node>& path,sf::RenderWindow& window) { //UWAGA na REFEREN
 int main()
 {
 
-	array<array<Node, columns>, rows> mapka;
+	std::array<std::array<Node, columns>, rows> mapka;
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			mapka[i][j] = Node(i, j);
-			if (i*j % (j+10) == 0 && i>5)
-				mapka[i][j].isObstacle = true; //dodawanie przeszkod
+			//if (i*j % (j+10) == 0 && i>5)
+			//	mapka[i][j].isObstacle = true; //dodawanie przeszkod
 			//if ((i + j) % 10 == 0)
 				//mapka[i][j].isCheckedByA_Star = true; //dodawanie sprawdzenia czy a* iterowal po danym node
 		}
 	}
+	//std::array<std::array<Node, columns>, rows> nowe = mapka;
 
+	//Node& poczatek = mapka[7][7];
+	//Node& destination = mapka[20][20];
+	std::vector<Node> wyznaczoa_przez_a_star;
 
 	//test przykladowej sciezki:
 	mapka[12][10].parentX = 5;
@@ -75,7 +80,7 @@ int main()
 
 	mapka[2][2].parentX = 2;
 	mapka[2][2].parentY = 10;
-	vector<Node> test_path;
+	std::vector<Node> test_path;
 	test_path.push_back(mapka[12][10]);
 	test_path.push_back(mapka[5][8]);
 	test_path.push_back(mapka[2][2]);
@@ -88,15 +93,26 @@ int main()
 	int current_mouse_pos_x=1;
 	int current_mouse_pos_y=1;
 
-
+	std::vector<Node> path_designed_by_A_star;
 	while (window.isOpen())
 	{
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
+
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A)
+			{
+				std::cout << "wcisnieto A: " << std::endl;
+				//path_designed_by_A_star = my_A_Star(mapka, poczatek, destination);
+				//wyznaczoa_przez_a_star = my_A_Star(mapka, mapka[5][3], mapka[10][10]);
+				my_A_Star(mapka, mapka[5][3], mapka[10][10]);
+				//my_A_Star(mapka, mapka[7][7], mapka[20][20]);
+				//testowane(mapka,poczatek,destination);
+			}
 		}
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {//ustawianie przeszkod
+			
 			mapka[x_from_pixel(sf::Mouse::getPosition(window).x)][y_from_pixel(sf::Mouse::getPosition(window).y)].isObstacle
 				= true;
 		}
@@ -108,6 +124,7 @@ int main()
 
 
 		draw_map(mapka, window);
+		
 		draw_path(test_path, window);
 		window.display();
 		
