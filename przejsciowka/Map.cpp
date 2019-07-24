a4943f17bd04203a9b20e4d02d5a118017b35a00
Damian Wysokinski <damianwysokinski@o2.pl>
2019-07-24 22:21:47 +0200

    sprzatanie kodu v2

--- przejsciowka/Map.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "Map.h"
-
-Map::Map(int width, int height, int square_size)
-{
-	width_ = width;
-	height_ = height;
-	square_size_ = square_size;
-}
--- przejsciowka/a_star_14_07_d.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "A_star.h"
-#include "window_dimensions.h"
-#include "Node.h"
-#include <vector>
-#include <array>
-
-
