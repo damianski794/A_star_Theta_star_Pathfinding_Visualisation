79da7b77a60787b8a64cfd189de0c9e7920cd695
Damian Wysokinski <damianwysokinski@o2.pl>
2019-09-12 22:23:24 +0200

    project done

--- przejsciowka/A_star.h
+++ przejsciowka/A_star.h
@@ -21,42 +21,21 @@ float euclidean_distance1(Node& current_node, Node& destination) {
 
 void draw_path(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)>& mapka, Node destination, sf::RenderWindow& window) {
 	float dlugosc_sciezki = 0;
-	//z innego algorytmu:
-	//for (int i = 0; i < 12; i++) {
-	//	for (int j = 0; j < 12; j++) {
-	//		std::cout << "" << i << "," << j << " --> parent " << mapka[i][j].parentX << "," << mapka[i][j].parentY << std::endl;
-	//	}
-	//}
-	//std::cout << std::endl << std::endl;
-
-	//sf::VertexArray lines(sf::LinesStrip, 2);
+	
 	int X = destination.x;
 	int Y = destination.y;
 	
-	//std::cout << "test" << std::endl;
-	//std::cout << "rysuje droge z: (" << mapka[X][Y].x << "," << mapka[X][Y].y << ") do (" << mapka[X][Y].parentX << "," << mapka[X][Y].parentY << ")" << std::endl;
+	
 	while (mapka[X][Y].y != -1  && !(mapka[X][Y].x == mapka[X][Y].parentX && mapka[X][Y].y == mapka[X][Y].parentY) && mapka[X][Y].x != -1) { //zmienione
-		//lines[0].position = sf::Vector2f(pixel_x(mapka[X][Y].x), pixel_y(mapka[X][Y].y));
-		//lines[0].color = sf::Color::Black;
-		//lines[1].position = sf::Vector2f(pixel_x(mapka[X][Y].parentX), pixel_y(mapka[X][Y].parentY));
-		//lines[1].color = sf::Color::Black;
-
+	
 		mapka[X][Y].shape.setFillColor(sf::Color::Green);
-		//window.draw(mapka[X][Y]);
+		
 		mapka[mapka[X][Y].parentX][mapka[X][Y].parentY].shape.setFillColor(sf::Color::Green);
-		//window.draw(mapka[mapka[X][Y].parentX][mapka[X][Y].parentY]);
-
-
-		//sf::RectangleShape line(sf::Vector2f(euclidean_distance1(mapka[X][Y], mapka[mapka[X][Y].parentX][mapka[X][Y].parentY]), 50));
-		//line.rotate(90);
-		//line.setFillColor(sf::Color::Green);
-		//line.setOrigin
-		//window.draw(line);
 
 		float single_line_dist = X_STEP * euclidean_distance1(mapka[X][Y], mapka[mapka[X][Y].parentX][mapka[X][Y].parentY]);
 		sf::RectangleShape line2(sf::Vector2f(single_line_dist, 5));
 
-		float line_y = (Y - mapka[X][Y].parentY); // - bo w sfml y zwieksza sie w dol ekranu
+		float line_y = (Y - mapka[X][Y].parentY); 
 		float line_x = (X - mapka[X][Y].parentX);
 
 		line2.rotate(atan2(line_y,line_x)*180/3.14);
@@ -65,21 +44,11 @@ void draw_path(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)>&
 		line2.setFillColor(sf::Color::Green);
 		window.draw(line2);
 
-		
-		//std::cout << "X,Y: " << mapka[X][Y].x << "," << mapka[X][Y].y << " --> " << mapka[X][Y].parentX << "," << mapka[X][Y].parentY << "\t";
-		//std::cout << "zostanie_dodane: " << euclidean_distance1(mapka[X][Y], mapka[mapka[X][Y].parentX][mapka[X][Y].parentY]) << std::endl;
 		dlugosc_sciezki += euclidean_distance1(mapka[X][Y], mapka[mapka[X][Y].parentX][mapka[X][Y].parentY]);
 
 		int tempX = mapka[X][Y].parentX;
 		Y = mapka[X][Y].parentY;
-		X = tempX;
-
-		//std::cout << "nowe X,Y: " <<X<<","<<Y<< std::endl;
-	//	window.draw(line, 2, sf::Lines);
-	
-
-		//window.draw(lines); 
-		
+		X = tempX;	
 	}
 	window.display();
 
@@ -90,38 +59,7 @@ void draw_path(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)>&
 
 }
 
-static std::vector<Node> makePath(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)> map, Node dest) {
-	try {
-		std::cout << "Found a path" << std::endl;
-		int x = dest.x;
-		int y = dest.y;
-		std::stack<Node> path;
-		std::vector<Node> usablePath;
 
-		while (!(map[x][y].parentX == x && map[x][y].parentY == y)
-			&& map[x][y].x != -1 && map[x][y].y != -1)
-		{
-			path.push(map[x][y]);
-			int tempX = map[x][y].parentX;
-			int tempY = map[x][y].parentY;
-			x = tempX;
-			y = tempY;
-
-		}
-		path.push(map[x][y]);
-
-		while (!path.empty()) {
-			Node top = path.top();
-			path.pop();
-			usablePath.emplace_back(top);
-		}
-		return usablePath;
-	}
-	catch (const std::exception& e) {
-		std::cout << "wywalilo blad" << std::endl;
-		std::cout << e.what() << std::endl;
-	}
-}
 
 static void a_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)>& mapka, Node& current_Node, Node& destination_Node, sf::RenderWindow& window) {
 
@@ -171,14 +109,9 @@ static void a_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STE
 	while (!openset.empty()) {
 		std::sort(openset.begin(), openset.end(), compareNode_first_bigger_pointer);
 
-		//std::cout << "rozmiar openset" << openset.size() << std::endl;
-		//Node node = *openset.back(); //bylo wczesniej rbegin()
+
 		Node* node = openset.back();
-		//std::cout << "NAJMNIEJSZY FCOST: " << node->fCost << std::endl;
-		/*for (auto it = openset.begin(); it != openset.end();it++) {
-			std::cout << "Rozmiar opeset:"<<openset.size() << std::endl;
-			std::cout << "G: " << it->gCost << std::endl;
-		}*/
+	
 
 
 		//node->shape.setFillColor(sf::Color::Red);// 7.09.2019 //odkomentuj zeby zobaczyc ktory node jest akutalnie sprawdzany
@@ -188,7 +121,7 @@ static void a_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STE
 
 
 		if (isDestination(node->x, node->y, destination_Node)) {
-			//std::cout << "this is the destination" << std::endl;
+			
 
 			auto end_time = std::chrono::high_resolution_clock::now();
 			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
@@ -197,8 +130,7 @@ static void a_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STE
 			draw_path(mapka, *node, window);
 			return;
 		}
-		openset.pop_back(); //drop the last element
-		//std::cout << "rozmiar openset" << openset.size() << std::endl;
+		openset.pop_back(); 
 		closeSet[node->x][node->y] = true;
 
 		int i = node->x;
@@ -218,25 +150,7 @@ static void a_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STE
 
 					int i_new = i + newX;
 					int j_new = j + newY;
-					//std::cout << "Checking Node: (" << i_new << "," << j_new<<")" << std::endl;
-
-					//std::cout << "wartosc closeset: " << closeSet[i_new][j_new] << std::endl;
-					/*if (my_isDestination(mapka[i_new][j_new], destination_Node) == true) {
-						mapka[i_new][j_new].parentX = mapka[i][j].x;
-						mapka[i_new][j_new].parentY = mapka[i][j].y;
-
-						//ponizej 9.09.2019
-						std::cout << "you are at the destination, a_star found path" << std::endl;
-						//draw_path(mapka, mapka[i_new][j_new], window);
-						//return;
-
-						//check_predecessor(mapka, destination_Node);
-						//std::cout << "poprzednik: " << mapka[i_new][j_new].parentX << "," << mapka[i_new][j_new].parentY << std::endl;
-
-						//wyswietl_sciezke(mapka, destination_Node);
-						//reczne_sprawdzanie_rodzica(mapka, destination_Node);
-					}
-					*/
+					
 					if (closeSet[i_new][j_new] == false) {
 						float Fnew; //dodane
 
@@ -250,7 +164,7 @@ static void a_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STE
 						float tentative_g_score = node->gCost + dist_beetween_node_and_adjastend;
 
 						Fnew = tentative_g_score + mapka[i_new][j_new].hCost;
-						//std::cout << "tentative_g_score: " << tentative_g_score << std::endl;
+						
 						bool tentative_is_better = false;
 						bool is_in_openset = false;
 
@@ -260,20 +174,16 @@ static void a_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STE
 								break;
 							}
 						}
-						//std::cout << "is_in_openset: 1=tak, 0=nie: " << is_in_openset << std::endl;
-						//if (is_in_openset == true) {
-							//std::cout << "node jest juz   w openset" << std::endl;
-						//}
+						
 						if (is_in_openset == false) {
-							//std::cout << "dodano element" << std::endl;
-							//mapka[i_new][j_new].hCost = my_calcutateH(mapka[i_new][j_new], destination_Node);
+							
 							mapka[i_new][j_new].fCost = Fnew;
 							mapka[i_new][j_new].gCost = tentative_g_score;
 							mapka[i_new][j_new].parentX = mapka[i][j].x;
 							mapka[i_new][j_new].parentY = mapka[i][j].y;
 
 							tentative_is_better = true;
-							openset.push_back(&mapka[i_new][j_new]); //bylo emplace 
+							openset.push_back(&mapka[i_new][j_new]); 
 						}
 						else if (tentative_g_score < mapka[i_new][j_new].gCost) {
 							tentative_is_better = true;
--- przejsciowka/Theta_star.h
+++ przejsciowka/Theta_star.h
@@ -16,7 +16,7 @@ bool line_of_sight(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STE
 	int x1 = parent_Node.x, y1 = parent_Node.y;
 	int x2 = adjecent_Node.x, y2 = adjecent_Node.y;
 	int max = std::max(abs(x1 - x2), abs(y1 - y2));
-	max = 3 * max; //chyba nie potrzeba wiekszej dokladnosci
+	//max = 3 * max; //chyba nie potrzeba wiekszej dokladnosci
 	float t;
 	float x, y;
 	for (int i = 0; i <= max; i++) {
@@ -24,19 +24,16 @@ bool line_of_sight(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STE
 		x = (x1 * (1.0 - t) + x2 * t);
 		y = (y1 * (1.0 - t) + y2 * t);
 
-		//std::cout << "( " << round(x) << "=" << x << " , " << round(y) << "=" << y << " ) dla t = " << t << std::endl;
 		int y_new = round(y);
 		int x_new = round(x);
 		if (mapka[x_new][y_new].isObstacle) {
-			//std::cout << "wykryto przeszkode" << std::endl;
 			return false;
 		}
 
 		//mapka[x_new][y_new].shape.setFillColor(sf::Color::Magenta); //wylaczone wizualizacja sprawdzanych elementow
 		//window.draw(mapka[x_new][y_new].shape);
 		//window.display();
-		//if (i == max - 1)
-		//	std::cout << "KONIEC" << std::endl;
+		
 	}
 	return true;
 }
@@ -65,17 +62,7 @@ void update_vertex(Node* node,Node& adjecent, std::array<std::array<Node, (Y_MAX
 			return;
 		}
 	}
-	if (adjecent.parentX == -1 && adjecent.parentY == -1) {
-		//adjecent.parentX = node->x;
-		//adjecent.parentY = node->y;
-		//adjecent.gCost = node->gCost + euclidean_distance(*node, adjecent);
-		//adjecent.fCost = adjecent.gCost + adjecent.hCost;
-		//std::cout << "nowy element" << std::endl;
-
-		//std::cout << std::endl << std::endl << std::endl << std::endl;
-		//update_vertex(node, adjecent, mapka, is_in_openset, window); //????
-	}
-	//mozliwe, ze trzeba tu troche pogrzebac z nowym gCost i fCost
+	
 }
 
 
@@ -97,7 +84,7 @@ static void a_star_theta_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_
 	for (int i = 0; i < (X_MAX / X_STEP); i++) {
 		for (int j = 0; j < (Y_MAX / Y_STEP); j++) {
 			mapka[i][j].fCost = 10000; //tutaj zmienilem 18.08.2019// bylo FLT_MAX
-			mapka[i][j].gCost = 10000; //FLT_COS bylo wczesniej
+			mapka[i][j].gCost = 10000; //FLT_MAX bylo wczesniej
 			mapka[i][j].hCost = my_calcutateH(mapka[i][j], destination_Node); //zmienilem z FLT_MAX
 			mapka[i][j].parentX = -1;
 			mapka[i][j].parentY = -1;
@@ -112,33 +99,26 @@ static void a_star_theta_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_
 	current_Node.parentX = 0;
 	current_Node.parentY = 0;
 
-	std::vector<Node*> openset; //DODA£EM std::unique<Node> #unique
+	std::vector<Node*> openset;
 	mapka[current_Node.x][current_Node.y].gCost = 0;
-	//mapka[current_Node.x][current_Node.y].fCost = 0; //uwaga, moze byc bez sensu
-	openset.push_back(&mapka[current_Node.x][current_Node.y]); //DODA£EM & przed mapka #unique //push_back() moze ew. byc
 
-	//current_Node.gCost = 0;
+	openset.push_back(&mapka[current_Node.x][current_Node.y]);
+
+	
 	mapka[current_Node.x][current_Node.y].gCost = 0;
-	mapka[current_Node.x][current_Node.y].parentX = current_Node.parentX; //pamietaj zeby zmienic na to co wyzzej
+	mapka[current_Node.x][current_Node.y].parentX = current_Node.parentX; 
 	mapka[current_Node.x][current_Node.y].parentY = current_Node.parentY;
 
-	//bool found_path = false;
+	
 
 	while (!openset.empty()) {
 		std::sort(openset.begin(), openset.end(), compareNode_first_bigger_pointer);
 
-		//std::cout << "rozmiar openset" << openset.size() << std::endl;
-		//Node node = *openset.back(); //bylo wczesniej rbegin()
 		Node* node = openset.back();
-		/*for (auto it = openset.begin(); it != openset.end();it++) {
-			std::cout << "Rozmiar opeset:"<<openset.size() << std::endl;
-			std::cout << "G: " << it->gCost << std::endl;
-		}*/
-		//std::cout << "NAJMNIEJSZY FCOST: " << node->fCost <<"  a openlist zawiera: "<<openset.size()<<" elementow"<< std::endl;
+		
 
 		if (isDestination(node->x, node->y, destination_Node)) {
-			//std::cout << "this is the destination" << std::endl;
-
+			
 			auto end_time = std::chrono::high_resolution_clock::now();
 			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
 			std::cout << "Algorithm time (in miliseconds) is: " << double(duration.count()) / 1000 << std::endl;
@@ -147,12 +127,12 @@ static void a_star_theta_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_
 			return;
 		}
 
-		//node->shape.setFillColor(sf::Color::Red);// 7.09.2019 //odkomentuj zeby zobaczyc jakie nody sa aktualnie sprawdzane
+		//node->shape.setFillColor(sf::Color::Red);//odkomentuj zeby zobaczyc jakie nody sa aktualnie sprawdzane
 		//window.draw(node->shape);
 		//window.display();
 
-		openset.pop_back(); //drop the last element
-		//std::cout << "rozmiar openset" << openset.size() << std::endl;
+		openset.pop_back();
+		
 		closeSet[node->x][node->y] = true;
 
 		int i = node->x;
@@ -172,74 +152,8 @@ static void a_star_theta_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_
 
 					int i_new = i + newX;
 					int j_new = j + newY;
-					//std::cout << "Checking Node: (" << i_new << "," << j_new<<")" << std::endl;
-
-					//std::cout << "wartosc closeset: " << closeSet[i_new][j_new] << std::endl;
-					/*
-					if (my_isDestination(mapka[i_new][j_new], destination_Node) == true) {
-						mapka[i_new][j_new].parentX = mapka[i][j].x;
-						mapka[i_new][j_new].parentY = mapka[i][j].y;
-						std::cout << "you are at the destination, a_star found path" << std::endl;
-
-						//check_predecessor(mapka, destination_Node);
-						//std::cout << "poprzednik: " << mapka[i_new][j_new].parentX << "," << mapka[i_new][j_new].parentY << std::endl;
-
-						draw_path(mapka, mapka[i_new][j_new], window);
-						
-						return;
-					} */
+					
 					if (closeSet[i_new][j_new] == false) {
-						/* from here to::
-						float Fnew; //dodane
-
-						float dist_beetween_node_and_adjastend;
-						if ((newX == -1 && newY == -1) || (newX == -1 && newY == 1) || (newX == 1 && newY == -1) || (newX == 1 && newY == 1)) {
-							dist_beetween_node_and_adjastend = 1.4f;
-						}
-						else {
-							dist_beetween_node_and_adjastend = 1.0f;
-						}
-						float tentative_g_score = node->gCost + dist_beetween_node_and_adjastend;
-
-						Fnew = tentative_g_score + mapka[i_new][j_new].hCost;
-						//std::cout << "tentative_g_score: " << tentative_g_score << std::endl;
-						bool tentative_is_better = false;
-						bool is_in_openset = false;
-
-						for (auto it = openset.rbegin(); it != openset.rend(); ++it) { //sprawdzam czy sprawdzany node nie jest w openSet
-							if (**it == mapka[i_new][j_new]) {
-								is_in_openset = true;
-								//break;
-							}
-						}
-						//std::cout << "is_in_openset: 1=tak, 0=nie: " << is_in_openset << std::endl;
-						//if (is_in_openset == true) {
-							//std::cout << "node jest juz   w openset" << std::endl;
-						//}
-						if (is_in_openset == false) {
-							//std::cout << "dodano element" << std::endl;
-							mapka[i_new][j_new].hCost = my_calcutateH(mapka[i_new][j_new], destination_Node);
-							mapka[i_new][j_new].fCost = Fnew;
-							mapka[i_new][j_new].gCost = tentative_g_score;
-							mapka[i_new][j_new].parentX = mapka[i][j].x;
-							mapka[i_new][j_new].parentY = mapka[i][j].y;
-
-							tentative_is_better = true;
-							openset.push_back(&mapka[i_new][j_new]); //bylo emplace 
-						}
-						else if (tentative_g_score < mapka[i_new][j_new].gCost) {
-							tentative_is_better = true;
-						}
-
-						if (tentative_is_better) {
-							mapka[i_new][j_new].parentX = node->x;
-							mapka[i_new][j_new].parentY = node->y;
-							mapka[i_new][j_new].gCost = tentative_g_score;
-							mapka[i_new][j_new].fCost = mapka[i_new][j_new].gCost + mapka[i_new][j_new].hCost;
-						}
-						*/ //too here::
-
-
 						bool is_in_openset = false;
 
 						for (auto it = openset.rbegin(); it != openset.rend(); ++it) { //sprawdzam czy sprawdzany node nie jest w openSet
@@ -249,11 +163,6 @@ static void a_star_theta_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_
 							}
 						}
 						if (is_in_openset == false) {
-							//mapka[i_new][j_new].parentX = node->x;
-							//mapka[i_new][j_new].parentY = node->y;
-							//mapka[i_new][j_new].gCost = node->gCost + euclidean_distance(*node, mapka[i_new][j_new]);
-							//mapka[i_new][j_new].fCost = mapka[i_new][j_new].gCost + mapka[i_new][j_new].hCost;
-
 							openset.push_back(&mapka[i_new][j_new]);
 						}
 						update_vertex(node, mapka[i_new][j_new], mapka, is_in_openset, window);
@@ -269,33 +178,6 @@ static void a_star_theta_star(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_
 }
 
 
-static void update_vertex(std::array<std::array<Node, (Y_MAX / Y_STEP)>, (X_MAX / X_STEP)>& mapka, Node& current_Node, Node& destination_Node, sf::RenderWindow& window) {
-
-}
-
-/*
-				function update_vertex(s, neighbor)
-				// This part of the algorithm is the main difference between A* and Theta*
-				if line_of_sight(parent(s), neighbor)
-				// If there is line-of-sight between parent(s) and neighbor
-				// then ignore s and use the path from parent(s) to neighbor 
-				if gScore(parent(s)) + c(parent(s), neighbor) < gScore(neighbor)
-					// c(s, neighbor) is the Euclidean distance from s to neighbor
-					gScore(neighbor) := gScore(parent(s)) + c(parent(s), neighbor)
-					parent(neighbor) : = parent(s)
-					if neighbor in open
-						open.remove(neighbor)
-						open.insert(neighbor, gScore(neighbor) + heuristic(neighbor))
-					else
-						// If the length of the path from start to s and from s to 
-						// neighbor is shorter than the shortest currently known distance
-						// from start to neighbor, then update node with the new distance
-						if gScore(s) + c(s, neighbor) < gScore(neighbor)
-							gScore(neighbor) := gScore(s) + c(s, neighbor)
-							parent(neighbor) : = s
-							if neighbor in open
-								open.remove(neighbor)
-								open.insert(neighbor, gScore(neighbor) + heuristic(neighbor))
 
 
-*/
+
